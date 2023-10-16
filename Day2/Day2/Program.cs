8c8644b7d4af16db7db271c38f78125f214d24fc
LeeVays <kamil.ulinowicz@gmail.com>
2023-10-16 21:50:30 +0200

    Praca domowa dzień 4

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myName = "Kamil";
+string sex = "Male";
+int myAge = 24;
+
+if (sex == "Male"){
+    if (myAge < 30)
+    {
+        Console.WriteLine(sex + ", " + "ponizej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine(sex + ", " + "powyzej 30 lat");
+    }
+}
+else{
+    if (myAge < 30)
+    {
+        Console.WriteLine(myName + ", " + "ponizej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine(myName + ", " + "powyzej 30 lat");
+    }
+}
+    
+Console.WriteLine(myName + ", " + "lat " + myAge);
+
+if (myAge >= 18){
+    if (sex == "Male") {
+        Console.WriteLine("Pelnoletni mezczyna");
+    }
+    else {
+        Console.WriteLine("Pelnoletnia kobieta");
+    }
+}
+else{
+    if (sex == "Male"){
+        Console.WriteLine("Niepelnoletni mezczyna");
+    }
+    else{
+        Console.WriteLine("Niepelnoletnia kobieta");
+    }
+}
