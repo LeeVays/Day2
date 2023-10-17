fcc46507b7dcd1cbd5c0a53eb8e89ec45aa5c38e
LeeVays <kamil.ulinowicz@gmail.com>
2023-10-17 23:01:38 +0200

    Domowa dzień 6 bez wskazówki

---
+++ Day2/Day2/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Employee
+    {
+        private List<int> grade = new List<int>();
+
+        public Employee(string name, string surrname, int age)
+        {
+            this.name=name;
+            this.surrname = surrname;
+            this.age = age;
+        }
+        public string name {  get; private set; }
+        public string surrname { get; private set; }
+        public int age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.grade.Sum();
+            }
+        }
+
+        public void addGrade(int grade)
+        {
+            this.grade.Add(grade);
+        }
+    }
+}
--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,15 +1,39 @@
-﻿int number = 4566;
-string numberIsString = number.ToString();
-char[] letters = numberIsString.ToCharArray();
-int[] digitCount = new int[10];
+﻿using Day2;
+using System.ComponentModel.DataAnnotations;
 
-foreach (char letter in letters){
-    if (char.IsDigit(letter)){
-        int digit = int.Parse(letter.ToString());
-        digitCount[digit]++; 
-    }
-}
-for (int i = 0; i < 10; i++)
+Employee employee1 = new Employee("Kamil", "Nowak", 21);
+Employee employee2 = new Employee("Pawel", "Przybysz", 34);
+Employee employee3 = new Employee("Jan", "Niezbędny", 37);
+
+Random random = new Random();
+for (int i = 0; i < 5; i++)
 {
-    Console.WriteLine(i + " => " + digitCount[i]);
+    int number1 = random.Next(1, 11);
+    employee1.addGrade(number1);
+    int number2 = random.Next(1, 11);
+    employee2.addGrade(number2);
+    int number3 = random.Next(1, 11);
+    employee3.addGrade(number3);
+}
+var result1 = employee1.Result;
+var result2 = employee2.Result;
+var result3 = employee3.Result;
+
+Console.WriteLine("Wynik pierwszego pracownika "+result1);
+Console.WriteLine("Wynik drugiego pracownika " +result2);
+Console.WriteLine("Wynik trzeciego pracownika " +result3);
+
+var winner1 = Math.Max(result1, result2);
+var winner2 = Math.Max(winner1, result3);
+
+if(winner2 ==result1)
+{
+    Console.WriteLine("najwyższy wynik to " + winner2+" i należy do "+employee1.name+" "+employee1.surrname+" wiek "+employee1.age+" lat");
+}else if(winner2 == result2)
+{
+    Console.WriteLine("najwyższy wynik to " + winner2 + " i należy do " + employee2.name + " " + employee2.surrname + " wiek " + employee2.age + " lat");
+}
+else
+{
+    Console.WriteLine("najwyższy wynik to " + winner2 + " i należy do " + employee3.name + " " + employee3.surrname + " wiek " + employee3.age + " lat");
 }
